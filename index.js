8a9183396e75bd6e0016b57a064ae5394e6fc17c
vitor-driven <38925839+vitor-driven@users.noreply.github.com>
2023-06-24 19:08:43 -0300

    sign-up

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.listen(5000, () => {
+    console.log(`Usando porta 5000`);
+});
+
+const userList = [];
+const tweetList = [];
+
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body;
+
+    if (!username || !avatar) {
+        return res.status(400).send("Preencha todos os campos");
+    }
+    const newUser = { username, avatar };
+    userList.push(newUser);
+    res.status(200).send("OK");
+});
