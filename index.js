65f3af08b05aed78e73f2a64375219c392233065
vitor-driven <38925839+vitor-driven@users.noreply.github.com>
2023-06-24 19:54:50 -0300

    postar tweet

--- index.js
+++ index.js
@@ -12,13 +12,35 @@ app.listen(5000, () => {
 const userList = [];
 const tweetList = [];
 
+function userExists(username) {
+    return userList.some((user) => user.username === username);
+}
+
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
 
     if (!username || !avatar) {
+        // checa se todos os campos estão preenchidos
         return res.status(400).send("Preencha todos os campos");
     }
+
+    if (userExists(username)) {
+        // checa se o nome de usuario ja está em uso
+        return res.status(400).send("Usuario ja existe");
+    }
+
     const newUser = { username, avatar };
     userList.push(newUser);
-    res.status(200).send("OK");
+    return res.status(200).send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body;
+
+    if (!userExists(username)) {
+        return res.status(401).send("UNAUTHORIZED");
+    }
+    const newTweet = { username, tweet };
+    tweetList.push(newTweet);
+    return res.status(200).send("OK");
 });
